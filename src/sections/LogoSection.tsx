b50905b150abdce0aecadcdc21225c846b6b6d77
Subhakanta Roul <papu.roul99@gmail.com>
2026-01-13 23:50:06 +0530

    portfolio: tech icon, tech stack and logos

---
+++ src/sections/LogoSection.tsx
@@ -0,0 +1,32 @@
+import { logoIconsList } from '@/constants'
+import React from 'react'
+
+const LogoIcon = ({icon}) => {
+    console.log(icon)
+    return (
+        <div className="flex-none flex-center marquee-item">
+            <img src={icon.imgPath} alt={icon.imgpath} />
+        </div>
+    )
+}
+
+function LogoSection() {
+  return (
+    <div className='md:my-20 my-10 relative'>
+        <div className="gradient-edge"/>
+        <div className="gradient-edge"/>
+        <div className="marquee h-52">
+            <div className="marquee-box md:gap-12 gap-5">
+                {logoIconsList.map((icon, index) => (
+                    <LogoIcon key={index} icon={icon} />
+                ))}
+                {logoIconsList.map((icon, index) => (
+                    <LogoIcon key={index} icon={icon} />
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LogoSection
